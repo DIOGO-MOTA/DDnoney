c31a8f99ea1afb1a954aaedd4587e8860a21658c
DIOGO-MOTA <diogomota21@gmail>
2021-03-26 17:46:05 -0300

    Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+
 import logoImg from '../../assets/logo.svg';
+import NewTransactionModal from '../NewTransactionModal';
 
 import { Container, Content } from './styles';
 
+
 const Header: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+   
+  const handleOpenNewTransactionModal = useCallback(()=>{
+    setIsNewTransactionModalOpen(true);
+  },[]);
+
+  const handleCloseNewTransactionModal = useCallback(()=>{
+    setIsNewTransactionModalOpen(false);
+  },[]);
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dd money" />
-        <button type="button">Nova transação</button>
+        <button type="button" 
+        onClick={handleOpenNewTransactionModal}
+        >
+        Nova transação
+        </button>
+
+        <NewTransactionModal
+         isOpen={isNewTransactionModalOpen}
+         onRequestClose={handleCloseNewTransactionModal}/>
+       
       </Content>
    </Container>
   );
